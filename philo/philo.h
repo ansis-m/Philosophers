9a6fbcd8fb7515b4f3267e243a4083afe39ecfed
Ansis Maleckis <maleckisansis@gmail.com>
2021-12-31 10:43:33 +0200

    init

---
+++ philo/philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amalecki <amalecki@students.42wolfsburg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/29 11:33:50 by amalecki          #+#    #+#             */
+/*   Updated: 2021/12/29 11:38:25 by amalecki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include	<unistd.h>
+# include	<stdlib.h>
+# include	<stdbool.h>
+# include	<limits.h> // TO BE REMOVED????????
+# include	<sys/types.h>
+# include <stdio.h>
+# include <pthread.h>
+
+#endif
